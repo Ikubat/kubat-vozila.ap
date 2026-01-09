b436466ea44a13b8fc7578791aeafc19250df7f8
Ikubat <88620718+Ikubat@users.noreply.github.com>
2026-01-09 22:27:29 +0100

    Preserve partner fields when editing uplatnice

--- app/uplatnice.js
+++ app/uplatnice.js
@@ -780,6 +780,8 @@
 
       const uplatilacId = item.uplatilac_id || matchedUplatilac?.id || null;
       const primateljId = item.primatelj_id || matchedPrimatelj?.id || null;
+      const uplatilacPartner = matchedUplatilac || (uplatilacId ? state.partners.get(uplatilacId) : null);
+      const primateljPartner = matchedPrimatelj || (primateljId ? state.partners.get(primateljId) : null);
 
       setPartner(
         'uplatilac',
@@ -794,15 +796,31 @@
         matchedPrimatelj || null
       );
       if ($uplatilacTekst) $uplatilacTekst.value = item.uplatilac_tekst || '';
-      if ($uplatilacKontakt) $uplatilacKontakt.value = item.uplatilac_kontakt || '';
-      if ($uplatilacAdresa) $uplatilacAdresa.value = item.uplatilac_adresa || '';
-      if ($uplatilacMjesto) $uplatilacMjesto.value = item.uplatilac_mjesto || '';
-      if ($uplatilacIdBroj) $uplatilacIdBroj.value = item.uplatilac_id_broj || '';
+      if ($uplatilacKontakt) {
+        $uplatilacKontakt.value = item.uplatilac_kontakt || uplatilacPartner?.kontakt || '';
+      }
+      if ($uplatilacAdresa) {
+        $uplatilacAdresa.value = item.uplatilac_adresa || uplatilacPartner?.adresa || '';
+      }
+      if ($uplatilacMjesto) {
+        $uplatilacMjesto.value = item.uplatilac_mjesto || uplatilacPartner?.mjesto_naziv || '';
+      }
+      if ($uplatilacIdBroj) {
+        $uplatilacIdBroj.value = item.uplatilac_id_broj || uplatilacPartner?.id_broj || '';
+      }
       if ($primateljTekst) $primateljTekst.value = item.primatelj_tekst || '';
-      if ($primateljKontakt) $primateljKontakt.value = item.primatelj_kontakt || '';
-      if ($primateljAdresa) $primateljAdresa.value = item.primatelj_adresa || '';
-      if ($primateljMjesto) $primateljMjesto.value = item.primatelj_mjesto || '';
-      if ($primateljIdBroj) $primateljIdBroj.value = item.primatelj_id_broj || '';
+      if ($primateljKontakt) {
+        $primateljKontakt.value = item.primatelj_kontakt || primateljPartner?.kontakt || '';
+      }
+      if ($primateljAdresa) {
+        $primateljAdresa.value = item.primatelj_adresa || primateljPartner?.adresa || '';
+      }
+      if ($primateljMjesto) {
+        $primateljMjesto.value = item.primatelj_mjesto || primateljPartner?.mjesto_naziv || '';
+      }
+      if ($primateljIdBroj) {
+        $primateljIdBroj.value = item.primatelj_id_broj || primateljPartner?.id_broj || '';
+      }
       $svrhaSel.value = item.svrha_id || '';
 
       $svrha.value = item.svrha_tekst || '';
